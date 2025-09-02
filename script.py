e2d44f3aab6efb62b4ed2b1d5f71f93a96ec8a4e
Paulin <paulohspfc209@gmail.com>
2025-09-02 19:18:48 -0300

    Up

--- script.py
+++ script.py
@@ -10,17 +10,22 @@ def configurar_xrdp():
         subprocess.run(['sudo', 'systemctl', 'start', 'xrdp'], check=True)
         
         print("Servidor RDP instalado e configurado corretamente.")
+        return True
     except subprocess.CalledProcessError as e:
         print(f"Erro ao configura o servidor RDP: {e}")
-
-
+        return False
 
 def configurar_ubip():
     try:
         subprocess.run(['sudo', 'apt-get', 'install', '-y', 'ubip'], check=True)
         
         print("UBIP instalado e configurado corretamente.")
+        return True
     except subprocess.CalledProcessError as e:
         print(f"Erro ao configurar UBIP: {e}")
+        return False
+
+configurar_ubip()
+configurar_xrdp()
 
 sys.exit(0)
