828c052912725fea84f2f541ddc055c04908685d
Paulo Henrique <paulohspfc209@gmail.com>
2025-09-03 08:51:24 -0300

    Upload

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import sys
+import glob
 
 def configurar_xrdp():
     try:
@@ -17,21 +18,37 @@ def configurar_xrdp():
         print(f"Erro ao configura o servidor RDP: {e}")
         return False
 
-def configurar_usbip():
+def configurar_pacotes_usbip():
     try:
-        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'usbip'], check=True)
+        subprocess.run(['sudo', 'apt', 'install', 'linux-tools-virtual', 'hwdata'], check=True)
         
-        print("UBIP instalado e configurado corretamente.")
+        print("Pacotes necessarios para o USBIP instalados corretamente.")
         return True
     except subprocess.CalledProcessError as e:
-        print(f"Erro ao configurar UBIP: {e}")
+        print(f"Erro ao instalar pacotes: {e}")
         return False
+    
+def instalar_usbip():
+    try:
+        usbip_path = glob.glob('/usr/lib/linux-tools/*/usbip')[0]
+        print(f"Caminho do usbip encontrado: {usbip_path}")
+    except IndexError:
+        print("Erro: usbip não encontrado no sistema.")
+        return False
+
+    #Outra alternativa para criar caminho
+    try:
+        subprocess.run(['sudo', 'update-alternatives', '--install', '/usr/local/bin/usbip', 'usbip', usbip_path, '20'], check=True)
+        print("Caminho alternativo para usbip criado com sucesso.")
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao criar caminho alternativo para usbip: {e}")
+
     
 if __name__ == "__main__":
     if configurar_xrdp():
         print("\nConfiguração do XRDP concluída. Prosseguindo para o USBIP...")
-        if configurar_usbip():
-            print("\nTodas as configurações foram concluídas com sucesso. Finalizando o script.")
+        if configurar_pacotes_usbip():
+            print("\nTodas os pacotes necessários foram instalados com sucesso. Finalizando o script.")
             sys.exit(0)
         else:
             print("\nOcorreu um erro fatal na configuração do USBIP. O script será encerrado.")
